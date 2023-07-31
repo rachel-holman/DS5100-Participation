24d8b34061065bb92af0cfc9d52e62c1331a0066
rachel-holman <rachel.h123@icloud.com>
2023-07-31 10:47:28 -0400

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Module9',
+    version='1.0.0',
+    author='Rachel Holman',
+    author_email='dnw9qk@virginia.edu',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=['numpy', 'pandas'],
+)
